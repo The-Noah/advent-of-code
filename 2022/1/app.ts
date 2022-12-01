ef1e8113894dc5c046c2811b071eec43129731bf
The Noah <thenoahbz@gmail.com>
2022-12-01 17:49:44 -0600

    Cleanup 2022 day 1

--- 2022/1/app.ts
+++ 2022/1/app.ts
@@ -2,12 +2,7 @@ const input = await Deno.readTextFile("input.txt");
 
 const elves = input.split("\n\n").map((e) => e.split("\n").map(Number));
 const sums = elves.map((elf) => elf.reduce((acc, value) => acc + value, 0));
+const sorted = sums.sort((a, b) => b - a);
 
-const highest = sums.sort((a, b) => b - a)[0];
-
-console.log("Part 1:", highest);
-
-const second = sums.sort((a, b) => b - a)[1];
-const third = sums.sort((a, b) => b - a)[2];
-
-console.log("Part 2:", highest + second + third);
+console.log("Part 1:", sorted[0]);
+console.log("Part 2:", sorted.slice(0, 3).reduce((acc, value) => acc + value, 0));
