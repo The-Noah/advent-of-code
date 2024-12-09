a1b2f6b6b9a901d61b3ea84a534b51fa227c6ecc
The Noah <thenoahbz@gmail.com>
2024-12-09 01:18:19 -0600

    feat: add 2024-3

---
+++ 2024/day3/src/main.rs
@@ -0,0 +1,63 @@
+fn main() {
+  let input = include_str!("../input.txt").trim();
+
+  let mut part1 = 0;
+  let mut part2 = 0;
+
+  let mut enabled = true;
+
+  for line in input.lines().collect::<Vec<&str>>() {
+    let mut in_mul = false;
+    let mut x: Option<i32> = None;
+
+    let mut buffer = String::new();
+
+    for char in line.chars() {
+      buffer.push(char);
+
+      if in_mul {
+        if char == ',' && x.is_none() {
+          if let Ok(value) = buffer[..buffer.len() - 1].parse() {
+            x = Some(value);
+          } else {
+            in_mul = false;
+          }
+
+          buffer = String::new();
+        } else if char == ')' {
+          let y: i32 = if let Ok(value) = buffer[..buffer.len() - 1].parse() {
+            value
+          } else {
+            in_mul = false;
+            continue;
+          };
+
+          if let Some(x) = x {
+            part1 += x * y;
+
+            if enabled {
+              part2 += x * y;
+            }
+          } else {
+            unreachable!();
+          }
+
+          in_mul = false;
+        } else if !char.is_ascii_digit() {
+          in_mul = false;
+        }
+      } else if buffer.ends_with("mul(") {
+        in_mul = true;
+        x = None;
+        buffer = String::new();
+      } else if buffer.ends_with("do()") {
+        enabled = true;
+      } else if buffer.ends_with("don't()") {
+        enabled = false;
+      }
+    }
+  }
+
+  println!("Part 1: {}", part1);
+  println!("Part 2: {}", part2);
+}
